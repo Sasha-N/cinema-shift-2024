6e207ad4b7a1ee473f8defae9d142a1135e44a63
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2024-02-03 09:08:21 +0700

    fix: change labels

--- src/app/affiche/card.tsx
+++ src/app/affiche/card.tsx
@@ -24,11 +24,12 @@ export default function Card({ filmCard }: { filmCard: filmCard }) {
     const URL = `https://shift-backend.onrender.com` + filmCard.img;
     return (
         <div className={styles.container_card}>
-            <div className={styles.label}>
-                <p className={styles.genre}>{filmCard.genres}</p>
-                <p className={styles.release}>{filmCard.releaseDate}</p>
+            <div className={styles.cover} style={{ backgroundImage: "url(" + URL + ")" }}>
+                <div className={styles.label}>
+                    <p className={styles.genre}>{filmCard.genres[0]}</p>
+                    <p className={styles.release}>{filmCard.releaseDate}</p>
+                </div>
             </div>
-            <div className={styles.cover} style={{backgroundImage:"url(" + URL + ")"}}></div>
             <div className={styles.description}>
                 <p className={styles.name}>{filmCard.name}</p>
                 <p className={styles.rating}>Кинопоиск: {filmCard.userRatings.kinopoisk}</p>
