2f27b20bd46dda79bce2d43c6ab0ddba93a054f4
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2024-02-08 10:35:21 +0700

    add: video for landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 'use client'
 import styles from "./page.module.css";
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/affiche');
-  });
+  const router = useRouter()
 
   return (
     <main className={styles.main}>
+      <div className={styles.video} onClick={() => router.push('/affiche')}>
+        <div className={styles.overlay}>Посети волшебные миры</div>
+        <video autoPlay loop src={require('./videos/violet.mp4')} />
+      </div>
     </main>
   );
 }
